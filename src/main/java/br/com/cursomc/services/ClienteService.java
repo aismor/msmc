8816f630a7065f629473b0c0a0f42d12914654e0
bno <brunodemoraiss@gmail.com>
2021-03-12 22:16:24 -0300

    Endpoint /clientes/id disponivel

---
+++ src/main/java/br/com/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cursomc.domain.Cliente;
+import br.com.cursomc.repositories.ClienteRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) throws ObjectNotFoundException {
+		 Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		} 
+
+}
